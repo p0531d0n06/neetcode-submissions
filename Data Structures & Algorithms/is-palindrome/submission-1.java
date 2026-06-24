25248653d89bce45bbadc9738b4614b2e844b314
Anish Garg <anish21.code@gmail.com>
2026-06-24 19:40:01 +0100

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s == ""){
+            return true;
+        }
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        s = s.toLowerCase();
+
+        while(i<=j){
+            if(!Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+                continue;
+            }
+
+            if(!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+                continue;
+            }
+
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+
+            i+=1;
+            j-=1;
+        }
+
+        return true;
+    }
+}
