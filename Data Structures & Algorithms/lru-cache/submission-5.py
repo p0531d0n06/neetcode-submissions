0f25f1d724dfe077cd956214d59086d4dfb8fcc7
Anish Garg <anish21.code@gmail.com>
2026-08-23 15:35:54 +0100

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.py
@@ -0,0 +1,24 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+        self.lruKey = []
+
+    def get(self, key: int) -> int:
+        if key in self.cache.keys():
+            self.lruKey.remove(key)
+            self.lruKey.append(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache.keys():
+            self.lruKey.remove(key)
+        self.lruKey.append(key)
+        self.cache[key] = value
+        if(len(self.lruKey) > self.cap):
+            self.cache.pop(self.lruKey[0])
+            self.lruKey.pop(0)
+        
+        
